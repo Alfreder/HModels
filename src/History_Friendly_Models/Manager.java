5c0f86cf064461779ecc328463d6e23e5586bcac
tendanger <tendanger@outlook.com>
2019-03-14 21:48:40 +0800

    delete print

--- src/History_Friendly_Models/Manager.java
+++ src/History_Friendly_Models/Manager.java
@@ -63,7 +63,5 @@ public class Manager {
         //model_ch5.makeMultipleSimulation();
 
         System.out.println("Simulation Success !");
-        System.out.println("Simulation Success !");
-        System.out.println("Simulation Success !");
     }
 }
